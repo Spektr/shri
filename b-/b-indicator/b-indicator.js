9f9bc031233b0ba5ba250d2e5cce78eca5bedbbb
Spektr <spektr1987@rambler.ru>
2013-07-17 00:22:44 +0400

    create b-indicator (native js)

--- b-/b-indicator/b-indicator.js
+++ b-/b-indicator/b-indicator.js
@@ -1,3 +1,11 @@
+/**
+ * Constructor for processbar
+ *
+ * @constructor
+ * @param {DOM Element} obj     where will be append Indicator
+ * @param {Number} pointNum     how many points equal 100%
+ * @return {Object}             public methods for change processbar
+ */
 var Indicator = function (obj, pointNum){
     var self = document.createElement('div'),
         line = document.createElement('div'),
@@ -12,20 +20,17 @@ var Indicator = function (obj, pointNum){
     self.appendChild(percent);
     obj.appendChild(self);
 
-
     return {
-
         'change':function(num){
-            console.log(num);
+            num = Math.round(num);
             line.style.width = num+"%";
             percent.innerHTML = num+"%";
-            console.log(self);
         },
-
-        'autoChange':function(){
+        'autoChange':function(speed){
             var els = document.getElementsByTagName('*'),
                 elsLen=els.length,
-                count = 0;
+                count = 0,
+                retObj = this;
 
             for(var i=0;i<elsLen;i++){
                 if(/b-indicator__point/.test(els[i].className)){
@@ -33,7 +38,24 @@ var Indicator = function (obj, pointNum){
                 }
             }
 
-            this.change(count*100/pointNum);
+            if(speed){
+                var start = (line.style.width)?line.style.width.slice(0,-1):0,
+                    stop = Math.round(count*100/pointNum);
+
+                var interval = window.setInterval(function(){
+                    if(start<stop){
+                        start++;
+                    }else if(start>stop){
+                        start--;
+                    }else{
+                        window.clearInterval(interval);
+                    };
+                    retObj.change(start);
+                }, speed);
+            }else{
+                retObj.change(count*100/pointNum);
+            }
+
         }
     }
 }
--- b-/b-task/b-task.js
+++ b-/b-task/b-task.js
@@ -2,7 +2,7 @@ $(function(){
 
     //устанавливаем обработчик на полученные вопросы
     bSimulatedServer.getQuestionArray(createTaskMenu);
-    var rrr = new Indicator($('.b-index__process')[0], 3);
+
 
     function createTaskMenu(data){
         var list =[];
@@ -12,32 +12,33 @@ $(function(){
         }
 
         $('.b-task__menu').append(list);
+        var processbar = new Indicator($('.b-index__process')[0], len);
 
         //хэндл на кнопы
         $('.b-task__item').on('click', getQuestion);
+        function getQuestion(){
+            var index = $(this).index();
+            var question = bSimulatedServer.getQuestion(index);
+            var answerStatus = bSimulatedServer.setAnswer($('.b-task__slide_solution textarea').val(), index);
+            if(answerStatus>0){
+                var statusClass = "b-pic b-pic__shri b-pic__shri_correct b-indicator__point";
+            }else if(answerStatus<0){
+                var statusClass = "b-pic b-pic__shri b-pic__shri_wrong";
+            }else{
+                var statusClass = "";
+            }
 
-    }
+            if(!question){console.log("печаль с выбором запроса");}
+            $('.b-task__item_active').removeClass('b-task__item_active b-pic b-pic__shri b-pic__shri_correct b-pic__shri_wrong  b-indicator__point').addClass(statusClass);
+            $(this).addClass('b-task__item_active');
+            $('.b-task__slide_description').text(question['request']);
+            $('.b-task__slide_solution textarea').val(question['answer']);
 
-    function getQuestion(){
-        var index = $(this).index();
-        var question = bSimulatedServer.getQuestion(index);
-        var answerStatus = bSimulatedServer.setAnswer($('.b-task__slide_solution textarea').val(), index);
-        if(answerStatus>0){
-            var statusClass = "b-pic b-pic__shri b-pic__shri_correct b-indicator__point";
-        }else if(answerStatus<0){
-            var statusClass = "b-pic b-pic__shri b-pic__shri_wrong";
-        }else{
-            var statusClass = "";
+            processbar.autoChange(10);
         }
-
-        if(!question){console.log("печаль с выбором запроса");}
-        $('.b-task__item_active').removeClass('b-task__item_active b-pic b-pic__shri b-pic__shri_correct b-pic__shri_wrong  b-indicator__point').addClass(statusClass);
-        $(this).addClass('b-task__item_active');
-        $('.b-task__slide_description').text(question['request']);
-        $('.b-task__slide_solution textarea').val(question['answer']);
-
-        rrr.autoChange();
     }
+
+
 
 
 
