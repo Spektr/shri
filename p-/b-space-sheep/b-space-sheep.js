ad9215620df60978cf58bf6f468213c1f0729099
Spektr <spektr1987@rambler.ru>
2013-07-29 16:32:14 +0400

    add coment to space task

--- p-/b-space-sheep/b-space-sheep.js
+++ p-/b-space-sheep/b-space-sheep.js
@@ -1,20 +1,46 @@
-/** Классы */
-var allBodies = [],
-    currentObject={};
+/** Глобальные переменные */
+var allBodies = [],			//массив всех космических тел
+    currentObject={},		//управляемый объект (корабль)
+	space = (function(){	//берем дом элемент космоса
+		var nodes = document.getElementsByTagName('*');
+		for (i = 0; i<nodes.length; i++){
+			if (nodes[i].className.indexOf("b-space-sheep__field") >= 0){
+				return nodes[i];
+			}
+		}
+	})();
+
+/**
+ * Создает экземпляр космического объекта.
+ * @constructor
+ * @this {Object}
+ * @param {Object} spaceParams Набор параметров для создания обьекта.
+ * {string} name Название космообъекта.
+ * {number}[] posititon Местоположение космообъекта.
+ * {number} loadLevel Текущая загрузка объекта.
+ * {number} capacity Максимальная вместимость космообъекта.
+ * @return {object} Прототип космообъекта
+ */
 function SpaceObject(spaceParams){
-	var type=false,
+	var type=false,	//тип будущего объекта (неопределен)
 		name=spaceParams['name']||"Инкогнито",
 		position = spaceParams['position']||[0,0],
 		capacity = (spaceParams['capacity'] && spaceParams['capacity']>=0)?spaceParams['capacity']:1000000,
 		loadLevel = (spaceParams['loadLevel'] && spaceParams['loadLevel']<=capacity)?spaceParams['loadLevel']:0,
-		visualObject = document.createElement("span"),
-				
+		visualObject = document.createElement("span"),	//блок, который будет характеризовать объект в дом дереве
+		
 		/** getters & setters */
 		getName = function(){return name;},
 		getObject = function(){return visualObject;},
 		getType = function(){return type;},
 		setType = function(typeName){if((typeof typeName=='string') && !type){type=typeName;};},
 		getPosition = function(){return position;},
+		
+		/** 
+		* Устанавливает месторасположение объекта
+		* @param {number} x Позиция по горизонтали
+		* @param {number} y Позиция по вертикали
+		*/
 		setPosition = function(x,y){
 			x=(x)?x:position[0];
 			y=(y)?y:position[1];
@@ -22,9 +48,15 @@ function SpaceObject(spaceParams){
 			visualObject.style.left = x+"px";
 			visualObject.style.top = y+"px";
 		},
-		getOccupiedSpace = function(){return loadLevel;},
-		getAvailableAmountOfCargo = function(){return capacity;},
-		getFreeSpace = function(){return (capacity-loadLevel);},
+		getOccupiedSpace = function(){return loadLevel;},			//Выводит количество занятого места.
+		getAvailableAmountOfCargo = function(){return capacity;},	//Возвращает доступное количество места.
+		getFreeSpace = function(){return (capacity-loadLevel);},	//Выводит количество свободного места на корабле.
+		
+		/** 
+		* Проверяет находится ли текущий объект в той же точке, что и переданный в параметрах
+		* @param {object} SpaceObject Проверяемый космообъект
+		* @return {bool} Рядом или нет
+		*/
 		nearTo = function(SpaceObject){
 			var one = SpaceObject.getPosition(),
 				two = this.getPosition();
@@ -33,19 +65,22 @@ function SpaceObject(spaceParams){
 		
 		/**
 		 * Выводит текущее состояние объекта: тип, имя, местоположение, доступную грузоподъемность.
+		 * @param {bool} inText Возвращать ли текст или выводить в консоль 
+		 * @return {string|bool} Строка или утвердительный объект
 		 * @example
 		 * spaceObject.report(); // Грузовой корабль. Перехватчик №1. Местоположение: Земля. Товаров нет.
 		 * @example
-		 * spaceObject.report(); // Планета. Земля. Местоположение: 50,20. Количество груза: 200т.
+		 * spaceObject.report(); // Планета. Земля. Местоположение: 50,20. Количество груза: 20000т.
 		 * @name SpaceObject.report
 		 */		
 		report = function(inText){
+			/** Формируем строку ответа */
 			var rType="Тип: "+type+".<br /> ",
 				rName="Название: "+name+".<br /> ",
 				rCurrentPlace = "Местоположение: "+position+".<br />",
 				rCargo = "Груз: "+((loadLevel)?loadLevel+"т.":"отсутствует.")+"<br />";
 			
-			/** Корректируем позицию если на планете */
+			/** Корректируем вывод позиции если на планете */
 			for(key in allBodies){
 				var obj = allBodies[key],
 					pos = obj.getPosition(),
@@ -62,27 +97,35 @@ function SpaceObject(spaceParams){
 				return rType+rName+rCurrentPlace+rCargo;
 			}else{
 				console.log(rType+rName+rCurrentPlace+rCargo);
+				return true;
 			}
 		},
 		
-		/** общая функция загрузки выгрузки */
+		/**
+		 * Общий метод для загрузки и выгрузки товара с/на объект. 
+		 * (особенность: операция загрузки инициирует операцию разгрузки на паралельном объекте и наоборот)
+		 * @param {Object} SpaceObject С каким объектом будем взаимодействовать.
+		 * @param {number} cargoWeight Объём товаров.
+		 * @param {bool} loading Загружать или разгружать.
+		 * @return {number|bool} Объём реально обработанного товара или отказ
+		 */
 		commonLoad = function(SpaceObject, cargoWeight, loading){
-			if(!SpaceObject['getAvailableAmountOfCargo'] || cargoWeight<0){ return false;};									//проверка на адекватность аргументов
-			var answer = false,																								//ответ метода (реально обработанное карго)
-			postCount = (commonLoad.caller && (commonLoad.caller.toString() == commonLoad.toString()))?true:false,			//0_0 флаг рекурсивного вызова
-			available = (loading)?this.getFreeSpace():loadLevel,															//доступное место/товар (зависит от операции)
-			errorMessage = (loading)?"Вы реально желаете загрузиться по полной?":"Вы реально желаете забрать все товары?";	//сообщение при переполнении
+			if(!SpaceObject['getAvailableAmountOfCargo'] || cargoWeight<0){ return false;};										//проверка на адекватность аргументов
+			var answer = false,																									//ответ метода (реально обработанное карго)
+				postCount = (commonLoad.caller && (commonLoad.caller.toString() == commonLoad.toString()))?true:false,			//0_0 флаг рекурсивного вызова
+				available = (loading)?this.getFreeSpace():loadLevel,															//доступное место/товар (зависит от операции)
+				errorMessage = (loading)?"Вы реально желаете загрузиться по полной?":"Вы реально желаете забрать все товары?";	//сообщение при переполнении
 			
-			//
+			/** Если объём указанного товара больше того что можно обработать */
 			if(cargoWeight>available){
 				cargoWeight = (confirm(errorMessage))?available:0;
 			}
 			
-			
+			/** Если вызывается первый раз, тогда инвертировать операцию для противоположного объекта */
 			cargoWeight =(postCount)?cargoWeight:SpaceObject.commonLoad(this, cargoWeight, !loading);
-			answer = (loading)?cargoWeight:(-cargoWeight);
-			loadLevel+= answer;
-			return Math.abs(answer);
+			answer = (loading)?cargoWeight:(-cargoWeight);	//знак операции
+			loadLevel+= answer;								//сама операция
+			return Math.abs(answer);						//возвращяем абсолютное значение сколько было списано или прибавлено
 			
 		},
 		
@@ -102,38 +145,67 @@ function SpaceObject(spaceParams){
 			'commonLoad':commonLoad
 		};
 	
-	/** показ сообщений о состоянии*/
-	var messageObject = document.createElement("span");
-	messageObject.className = "b-space-sheep__object-message";
-	$('.b-space-sheep__field').append(messageObject);
-	visualObject.onmouseover = function(){
+	/** показ сообщений о состоянии */
+	var messageObject = document.createElement("span");			//дом элемент
+	messageObject.className = "b-space-sheep__object-message";	//оформление
+	space.appendChild(messageObject);							//добавляем в космос
+	visualObject.onmouseover = function(){						//появление
 		var pos = getPosition();
 		messageObject.innerHTML = report(1);
 		messageObject.style.display = "inline";
 		messageObject.style.left =pos[0]+50+"px";
 		messageObject.style.top = pos[1]-30+"px";
-	}
-	visualObject.onmouseout = function(){
+	}	
+	visualObject.onmouseout = function(){						//исчезновение
 		messageObject.innerText = "";
 		messageObject.style.display = "none";
 	}
-
-    allBodies.push(obj);
-	return obj
+	
+	
+    allBodies.push(obj);	//пушим в общий стак объектов
+	return obj;				//и возвращает
 }
 
-function Vessel(){
-	var me = SpaceObject(arguments[0]),
-		visualObject = me.getObject(),
-		timer;
 
-    $('.b-space-sheep__field').append(visualObject);
-    me.setType("Грузовой корабль");
-	me.setPosition();
+/**
+ * Создает экземпляр космического корабля.
+ * @constructor
+ * @extend SpaceObject()
+ * @param {Object} spaceParams Набор параметров для создания обьекта.
+ * @return {object} корабль
+ */
+function Vessel(){
+	var me = SpaceObject(arguments[0]),	//расширяем
+		visualObject = me.getObject(),	//получаем дом объект
+		timer;
+		
+	me.setType("Грузовой корабль");						//устанавливаем тип космообьекта
+    space.appendChild(visualObject);					//добавляем в космос
+	me.setPosition();									//размещяем дом объект в космосе
+	
+	/**
+	 * Метод для визуального полета корабля.
+	 * @param {SpaceObject|number[]} coords Объект или координаты куда лететь.
+	 * @param {number} speed Скорость перемещения по космосу в милисекундах
+	 */
 	me.flyTo=function(coords, speed){
 		var coords = (coords['getAvailableAmountOfCargo'])?coords.getPosition():coords,
 			x = coords[0],
-			y = coords[1];
+			y = coords[1],
+			curPos = me.getPosition();
+			
+		visualObject.className = "b-space-sheep__object-sheep";
+		var tempX = Math.abs(x-curPos[0]),
+			tempY = Math.abs(y-curPos[1]),
+			tempClass = "";
+		
+		if(tempX<=tempY){
+			tempClass=(y>curPos[1])?" b-space-sheep__object-sheep_bottom":"";
+		}else{
+			tempClass=(x>curPos[0])?" b-space-sheep__object-sheep_right":" b-space-sheep__object-sheep_left";
+		}
+		visualObject.className+= tempClass;
+		
 		window.setTimeout(
 			function caller() {
 				window.clearTimeout(timer);
@@ -151,8 +223,9 @@ function Vessel(){
 		);
 	};
 
+	/** донастраиваем визуальный обьект корабля */
     visualObject.className="b-space-sheep__object-sheep";
-    visualObject.onclick = function(){
+    visualObject.onclick = function(){	//при клике на корабль, делаем его текущим
         var e = event || window.event;
         currentObject = me;
         if (e.stopPropagation) {
@@ -162,30 +235,45 @@ function Vessel(){
         }
 
     }
-
-	me.constructor = arguments.callee;
+	
+	/** Убираем некоторые ненужные методы */
+	me.getAvailableAmountOfCargo =function(){};
+	me.getObject =function(){};
+	me.getName =function(){};
+	me.getType =function(){};
+	me.setType =function(){};
+	me.nearTo =function(){};
+	
+	me.constructor = arguments.callee;	//конструктор = Vessel
 	return me;
 }
 
+/**
+ * Создает экземпляр Планеты.
+ * @constructor
+ * @extend SpaceObject()
+ * @param {Object} spaceParams Набор параметров для создания обьекта.
+ * @return {object} Планета
+ */
 function Planet(){
-	var me = SpaceObject(arguments[0]),
-		visualObject = me.getObject();
-
-    $('.b-space-sheep__field').append(visualObject);
-	me.setType("Планета");
-    me.setPosition();
+	var me = SpaceObject(arguments[0]),	//расширяем
+		visualObject = me.getObject();	//получаем дом объект
+	
+	me.setType("Планета");								//устанавливаем тип
+    space.appendChild(visualObject);					//добавляем в космос
+    me.setPosition();									//размещяем дом объект в космосе
+	
+	/** уточняем методы для загрузки/разгрузки только с планеты */
     me.loadCargoTo = function(SpaceObject, cargoWeight){
         return (me.nearTo(SpaceObject))?me.commonLoad(SpaceObject, cargoWeight, true):false;
     };
     me.unloadCargoFrom = function(SpaceObject, cargoWeight){
         return (me.nearTo(SpaceObject))?me.commonLoad(SpaceObject, cargoWeight, false):false;
     };
-    me.setPosition =function(){} /** Если это солнце и оно не движется */
 
-
+	/** донастраиваем визуальный обьект планеты */
 	visualObject.className="b-space-sheep__object-planet";
-    /** летим текущим карабликом к планете */
-    visualObject.onclick = function(){
+    visualObject.onclick = function(){	//при клике на планету, быстро летим к ней текущим кораблем
         var e = event || window.event;
         currentObject.flyTo(me.getPosition(),3);
         if (e.stopPropagation) {
@@ -194,9 +282,7 @@ function Planet(){
             e.cancelBubble = true;
         }
     }
-
-    /** загружаемся товарами */
-    visualObject.oncontextmenu = function(){
+    visualObject.oncontextmenu = function(){	//загружаемся товарами
         var operation = confirm("Желаете затовариться?"),
             cargo = parseInt((operation)?prompt("Сколько загрузить?", 400):prompt("Ну тогда, сколько выгрузить?", 400));
         if(operation){
@@ -207,27 +293,43 @@ function Planet(){
 
         return false;
     }
+	
+	/** Убираем некоторые ненужные методы */
+    me.setPosition =function(){};
+	me.getObject =function(){};
+	me.getOccupiedSpace =function(){};
+	me.getFreeSpace =function(){};
+	me.getOccupiedSpace =function(){};
 
-	me.constructor = arguments.callee;
+	me.constructor = arguments.callee;	//конструктор = Planet
 	return me;
 }
 
+/**
+ * Заполняет контейнер появляющимися звездами.
+ * @param {DOMElement} container Элемент куда будут загружены звезды.
+ * @param {number} numStars Количество звезд.
+ * @param {number} speed Скорость анимации звезд.
+ * @param {number} delay Задержка между появлением новой звезды.
+ */
 function starObject(container, numStars, speed, delay){
     var i= 0;
-    window.setTimeout(function pull(){
+    window.setTimeout(function pull(){	//вызываем таймаут на создание звезд
         if(i>=numStars){return false;}
         i++;
 
-        var star = document.createElement("div"),
-            position= 0,
-            step = 7,
-            x = container.clientWidth,
-            y = container.clientHeight;
+        var star = document.createElement("div"),	//делаем обьект
+            position= 0,							//начальная позиция фона звезды
+            step = 7,								//шаг смещения в пикселях
+            x = container.clientWidth,				//ширина контейнера куда будут грузиться звезды
+            y = container.clientHeight,				//его высота
+			interval,								//интервал
+			timeout;								//таймаут
 
         star.className = "b-space-sheep__object-star";
         container.appendChild(star);
 
-        var interval = window.setInterval(function starAnimate(){
+        interval = window.setInterval(function starAnimate(){	//вызываем интервал на анимацию звезд
             position-=step;
             star.style.backgroundPosition=position+"px";
             if(position==-28){step*=-1;}
@@ -238,29 +340,33 @@ function starObject(container, numStars, speed, delay){
             }
         },speed);
 
-        var timeout = window.setTimeout(pull, delay);
+        timeout = window.setTimeout(pull, delay);
     },0);
 }
-//window.clearInterval(interval);
+
 $(function(){
 
     /** выставляем две планеты */
-	var earth = Planet({'name':"Земля",'position':[80,20], 'loadLevel':10000});
-	var mars = Planet({'name':"Марс",'position':[180,200], 'loadLevel':10000});
+	var earth = Planet({'name':"Земля",'position':[80,20], 'loadLevel':30000});
+	var mars = Planet({'name':"Марс",'position':[180,200], 'loadLevel':9000});
 
-    /** создаем корабли */
-    $('.b-space-sheep__create-sheep').on('click', function(){
-        var sheep = Vessel({'name':"Космическая овца №"+allBodies.length,'position':[0,0], 'capacity':1000});
+    /** делаем кнопку создания кораблей */
+	var button = document.createElement("span");
+	button.className="b-space-sheep__create-sheep";
+	button.innerHTML = "Создать кораблик";
+	button.onclick = function(){
+        var sheep = Vessel({'name':"Космическая овца №"+allBodies.length,'position':[0,0], 'capacity':11000});
         currentObject = sheep;
-    });
+    };
+	space.appendChild(button);
 
     /** анимируем космос */
-    starObject($('.b-space-sheep__field')[0], 100, 1000, 300);
+    starObject(space, 100, 1000, 300);
 
     /** учим корабли летать */
-    $('.b-space-sheep__field').on('click', function(){
+    space.onclick = function(){
         var e = event || window.event;
         currentObject.flyTo([e.offsetX, e.offsetY],30);
-    });
+    };
 
 });
